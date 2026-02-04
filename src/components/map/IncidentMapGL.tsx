fc55b98ad4fee38b000171bb617bf5c6fecfd024
peja <pejastudios@gmail.com>
2026-02-04 18:12:16 +0100

    fix map2

--- src/components/map/IncidentMapGL.tsx
+++ src/components/map/IncidentMapGL.tsx
@@ -391,32 +391,27 @@ export default function IncidentMapGL({
         onMove={evt => setViewState(evt.viewState)}
         style={{ width: "100%", height: "100%" }}
         mapStyle={{
-          version: 8,
-          sources: {
-            osm: {
-              type: "raster",
-              tiles: [
-                "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              ],
-              tileSize: 256,
-              attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            },
-          },
-          layers: [
-            {
-              id: "osm",
-              type: "raster",
-              source: "osm",
-              paint: {
-                "raster-saturation": -0.5,
-                "raster-brightness-min": 0.1,
-                "raster-brightness-max": 0.4,
-              },
-            },
-          ],
-        }}
+  version: 8,
+  sources: {
+    osm: {
+      type: "raster",
+      tiles: [
+        "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      ],
+      tileSize: 256,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    },
+  },
+  layers: [
+    {
+      id: "osm",
+      type: "raster",
+      source: "osm",
+    },
+  ],
+}}
         maxZoom={18}
         minZoom={3}
       >
