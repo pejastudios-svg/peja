7c0a5141301f9ad112552d7341bf10ae984cf139
peja <pejastudios@gmail.com>
2026-02-08 11:46:05 +0100

    spaceagain

--- src/components/navigation/CapacitorInit.tsx
+++ src/components/navigation/CapacitorInit.tsx
@@ -22,7 +22,7 @@ export function CapacitorInit() {
     // so we use a known safe value for Android status bars
     document.documentElement.style.setProperty(
       "--cap-status-bar-height",
-      "64px"
+      "32px"
     );
 
     // Try to style the status bar if plugin is available
