897d9d5d695103ff3ca7f88bc4c443376c2fe9a1
peja <pejastudios@gmail.com>
2026-02-09 07:41:11 +0100

    Add offline page and update capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,7 @@ const config: CapacitorConfig = {
   server: {
     url: "https://peja.vercel.app",
     cleartext: false,
+    errorPath: "/offline.html",
   },
   android: {
     webContentsDebuggingEnabled: false,
